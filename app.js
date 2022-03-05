1c4a3bc2456a917d53fd4cb3311c92f9adb40d5f
Pinpong Tongpat <apa.pinpong.t@siampiwat.com>
2022-03-05 20:11:20 +0700

    001

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('test Hello World!'); 
+  res.write('001 test Hello World!'); 
   res.end(); 
 }).listen(8080, '0.0.0.0'); 
 
